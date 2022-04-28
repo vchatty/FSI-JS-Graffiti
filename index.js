6eaa93ea45927b682c419c5f38354e01313bff83
Vivecca Chatila <viveccachatila@gmail.com>
2022-04-28 19:38:07 -0400

    fixed errors

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
+let header = document.querySelector('#page-header')
 
-// Select some elements...
+header.style.textAlign = "left";
+header.style.color = 'hotpink';
+header.style.fontFamily = 'Impact';
+
+let dogImages = document.querySelectorAll('.dog-image')
+
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '50px'; 
+		dogImages[i].style.border = '10px solid hotpink';
+}
+
+let dogNames = document.querySelectorAll('.dog-name')
+	for(let i=0; i < dogNames.length; i++){
+		dogNames[i].style.color= 'hotpink'
+	}
+
+let pip = document.querySelector('.pip')
+	pip.style.color= 'blue'
+	
+
+let footerText = document.querySelectorAll('.footer')
+
+for(let i = 0; i < footerText.length; i++) {
+	footerText[i].style.color= 'hotpink'; 
+	footerText[i].style.fontFamily= 'Impact'; 
+}
+
+let dogImgSize = document.querySelector('.imagesize')
+
+for(let i = 0; i < dogImgSize.length; i++){
+	dogImgSize[i].style.border = '2px solid hotpink';
+}
